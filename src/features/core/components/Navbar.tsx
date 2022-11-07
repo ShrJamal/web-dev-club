13977c93af117a496dc572c1d53274d72c83943a
ShrJamal <me@shrjamal.com>
2022-11-08 00:05:45 +0100

    add navbar items

--- src/features/core/components/Navbar.tsx
+++ src/features/core/components/Navbar.tsx
@@ -1,10 +1,19 @@
 import ThemeSwitcher from './ThemeSwitcher'
 import Logo from './Logo'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <nav className="m-2 p-2 bg-base-300 flex items-center shadow-lg rounded-box">
       <Logo />
+      <div className="mx-8 space-x-4">
+        <Link href="/" className="link link-hover">
+          Home
+        </Link>
+        <Link href="/subjects" className="link link-hover">
+          Subjects
+        </Link>
+      </div>
       <div className="flex-1" />
       <ThemeSwitcher />
     </nav>
