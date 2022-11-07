81f6bca032d1673a622bc85b41238606f68092dd
ShrJamal <me@shrjamal.com>
2022-11-07 23:50:33 +0100

    [theme] fix and refactor logic

--- src/features/core/components/ThemeSwitcher.tsx
+++ src/features/core/components/ThemeSwitcher.tsx
@@ -1,25 +1,30 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { useEffect } from 'react'
 import { FaMoon, FaSun } from 'react-icons/fa'
 
 export default function ThemeSwitcher() {
-  const [isDark, setIsDark] = useState(true)
+  const [isDark, setIsDark] = useState(false)
+  const onChange = useCallback(
+    (theme?: string) => {
+      theme ??= 'light'
+      const dark = theme === 'dark'
+      localStorage.setItem('theme', theme)
+      document.documentElement.setAttribute('data-theme', theme)
+      if (isDark === dark) return
+      setIsDark(!isDark)
+    },
+    [isDark],
+  )
   useEffect(() => {
     if (typeof window === 'undefined') return
-    const dark = localStorage.getItem('theme') != 'light'
-    if (isDark == dark) return
-    setIsDark(!isDark)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    onChange(localStorage.getItem('theme') || undefined)
+  }, [onChange])
 
-  function onChange() {
-    const theme = isDark ? 'light' : 'dark'
-    localStorage.setItem('theme', theme)
-    document.documentElement.setAttribute('data-theme', theme)
-    setIsDark(!isDark)
-  }
   return (
-    <button className="btn btn-ghost" onClick={onChange}>
+    <button
+      className="btn btn-ghost"
+      onClick={() => onChange(isDark ? 'light' : 'dark')}
+    >
       {isDark ? <FaMoon /> : <FaSun />}
     </button>
   )
